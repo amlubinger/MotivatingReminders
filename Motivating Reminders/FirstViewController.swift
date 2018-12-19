afd4aa0bfd8329d0bb624e343e475eefb7f07a73
Andrew Lubinger <amlubinger@gmail.com>
2018-12-19 17:21:52 -0500

    Initial Commit

---
+++ Motivating Reminders/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Motivating Reminders
+//
+//  Created by Andrew Lubinger on 12/19/18.
+//  Copyright © 2018 Andrew Lubinger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Motivating RemindersTests/Motivating_RemindersTests.swift
@@ -0,0 +1,34 @@
+//
+//  Motivating_RemindersTests.swift
+//  Motivating RemindersTests
+//
+//  Created by Andrew Lubinger on 12/19/18.
+//  Copyright © 2018 Andrew Lubinger. All rights reserved.
+//
+
+import XCTest
+@testable import Motivating_Reminders
+
+class Motivating_RemindersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
